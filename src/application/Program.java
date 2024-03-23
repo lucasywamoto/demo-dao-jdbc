d4ce41be6acfbbe84bfb5504e832aaf8e657bb00
Lucas Ywamoto <92839243+lucasywamoto@users.noreply.github.com>
2024-03-23 12:40:39 -0400

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,5 +23,11 @@ public class Program {
 		for(Seller s : list) {
 			System.out.println(s);
 		}
+				
+		System.out.println("\n=== TEST 3: seller findAll ===");
+		list = sellerDao.findAll();
+		for(Seller s : list) {
+			System.out.println(s);
+		}
 	}
 }
