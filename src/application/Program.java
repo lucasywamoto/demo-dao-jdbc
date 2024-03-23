066da7664ebdd65d1c14c60f16521042e28763d2
Lucas Ywamoto <92839243+lucasywamoto@users.noreply.github.com>
2024-03-23 13:27:38 -0400

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,15 +31,19 @@ public class Program {
 			System.out.println(s);
 		}
 		
-		System.out.println("\n=== TEST 4: seller insert ===");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, dep);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
+//		System.out.println("\n=== TEST 4: seller insert ===");
+//		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, dep);
+//		sellerDao.insert(newSeller);
+//		System.out.println("Inserted! New id = " + newSeller.getId());
 		
-		System.out.println("\n=== TEST 4: seller update ===");
+		System.out.println("\n=== TEST 5: seller update ===");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update complete.");
+		
+		System.out.println("\n=== TEST 6: seller delete ===");
+		sellerDao.deleteById(10);
+		System.out.println("Deleted.");
 	}
 }
